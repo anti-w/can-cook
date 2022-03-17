0e4a0b3802f01d96c7aeadd066b47f37fb6f23ab
Wendel Demétrio <hey.wendel95@gmail.com>
2022-03-17 09:08:59 -0300

    migrated bot to mongoDB (almost done)

--- crawler.js
+++ crawler.js
@@ -12,15 +12,14 @@ async function crawler(client) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  page.exposeFunction("createFood", createFood);
   await page.goto(
     `http://www.tbca.net.br/base-dados/composicao_alimentos.php?pagina=1`
   );
 
   const content = await page.evaluate(() => {
-    const cod = [];
-    const name = [];
-    const group = [];
+    let cod = [];
+    let name = [];
+    let group = [];
     const itensPerPage = document.documentElement.querySelectorAll("tbody tr");
 
     itensPerPage.forEach((node) => {
@@ -35,6 +34,8 @@ async function crawler(client) {
     };
   });
 
+  page.exposeFunction("createFood", createFood);
+
   for (let i = 0; i < content.cod.length; i++) {
     let cod = content.cod[i];
     let name = content.name[i];
@@ -43,6 +44,41 @@ async function crawler(client) {
     await page.goto(
       `http://www.tbca.net.br/base-dados/int_composicao_alimentos.php?cod_produto=${cod}`
     );
+    const nutrientsPage = await page.evaluate(() => {
+      let components = [];
+      let values = [];
+      const itensPerPage =
+        document.documentElement.querySelectorAll("tbody tr");
+
+      itensPerPage.forEach((node) => {
+        let component = node.childNodes.item(0).innerText.replaceAll(" ", "");
+
+        let unity = node.childNodes.item(1).innerText;
+        let valuePer100g = node.childNodes.item(2).innerText.replace(",", ".");
+        valuePer100g = parseFloat(valuePer100g);
+
+        components.push(
+          component + unity //componente + unidade (açúcarg)
+        );
+        values.push(valuePer100g);
+      });
+
+      return { components, values };
+    });
+
+    let components = nutrientsPage.components;
+    let values = nutrientsPage.values;
+
+    const food = {
+      nome: name,
+      codigo: cod,
+      grupoAlimentar: group,
+      nutrientes: {},
+    };
+    for (let x = 0; x < components.length; x++) {
+      food.nutrientes[components[x]] = values[x];
+    }
+    createFood(food);
   }
 }
 
--- mongoDB.js
+++ mongoDB.js
@@ -1,11 +1,12 @@
 /* 
 may be helpfull ~> https://www.mongodb.com/blog/post/quick-start-nodejs-mongodb-how-to-get-connected-to-your-database
 */
+require("dotenv/config");
 const crawler = require("./crawler");
 const { MongoClient } = require("mongodb");
 
 async function main() {
-  const uri = `mongodb+srv://antiw:170195-+cent@cluster0.o6yj8.mongodb.net/can-eat?retryWrites=true&w=majority`;
+  const uri = `mongodb+srv://${process.env.MONGODB_CREDENTIALS}@cluster0.o6yj8.mongodb.net/can-eat?retryWrites=true&w=majority`;
   const client = new MongoClient(uri);
 
   try {
